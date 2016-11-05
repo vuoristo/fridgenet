e481a36e351300c4ec175fcea43fe2932c3968b7
Kenneth Blomqvist <kekeblom@gmail.com>
2016-11-05 14:27:53 +0200

    More robust image downloader

--- download.py
+++ download.py
@@ -5,6 +5,8 @@ import argparse
 import requests
 import io
 import hashlib
+import itertools
+import base64
 from PIL import Image
 from multiprocessing import Pool
 from selenium import webdriver
@@ -12,6 +14,7 @@ from selenium import webdriver
 argument_parser = argparse.ArgumentParser(description='Download images using google image search')
 argument_parser.add_argument('query', metavar='query', type=str, help='The query to download images from')
 argument_parser.add_argument('--count', metavar='count', default=100, type=int, help='How many images to fetch')
+argument_parser.add_argument('--label', metavar='label', type=str, help="The directory in which to store the images (images/<label>)", required=True)
 
 
 def ensure_directory(path):
@@ -33,33 +36,60 @@ def largest_file(dir_path):
 def fetch_image_urls(query, images_to_download):
     image_urls = set()
 
-    search_url = "https://duckduckgo.com/?q={q}&t=ht&iax=1&ia=images"
+    search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&biw=1680&bih=950&q={q}&oq={q}&gs_l=img.3..0l10.610.1292.0.1502.6.5.0.0.0.0.107.431.4j1.5.0....0...1ac.1.64.img..1.5.429.YCogLtkPTLU"
+    # search_url = "https://duckduckgo.com/?q={q}&t=ht&iax=1&ia=images"
     browser = webdriver.Firefox()
     browser.get(search_url.format(q=query))
-
-    time.sleep(3)
-
-
-    while len(image_urls) < images_to_download:
+    def scroll_to_bottom():
         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(5)
-        images = browser.find_elements_by_css_selector("img.tile--img__img")
+        time.sleep(2)
+
+    image_count = len(image_urls)
+    delta = 0
+    while image_count < images_to_download:
+        print("Found:", len(image_urls), "images")
+        scroll_to_bottom()
+
+        images = browser.find_elements_by_css_selector("img.rg_ic")
         for img in images:
             image_urls.add(img.get_attribute('src'))
+        delta = len(image_urls) - image_count
+        image_count = len(image_urls)
+
+        if delta == 0:
+            print("Can't find more images")
+            break
+
+        fetch_more_button = browser.find_element_by_css_selector(".ksb._kvc")
+        if fetch_more_button:
+            browser.execute_script("document.querySelector('.ksb._kvc').click();")
+            scroll_to_bottom()
 
     browser.quit()
     return image_urls
 
+def persist_image(dir_image_src):
+    label_directory = dir_image_src[0]
+    image_src = dir_image_src[1]
 
-
-def persist_image(image_url):
     size = (256, 256)
-    image_content = requests.get(image_url).content
+    try:
+        image_content = requests.get(image_src).content
+    except requests.exceptions.InvalidSchema:
+        # image is probably base64 encoded
+        image_data = re.sub('^data:image/.+;base64,', '', image_src)
+        image_content = base64.b64decode(image_data)
+    except Exception as e:
+        print("could not read", e, image_src)
+        return False
+
     image_file = io.BytesIO(image_content)
-    image = Image.open(image_file)
+    image = Image.open(image_file).convert('RGB')
     resized = image.resize(size)
-    with open(query_directory + hashlib.sha1(image_content).hexdigest(), 'wb')  as f:
+    with open(label_directory + hashlib.sha1(image_content).hexdigest(), 'wb')  as f:
         resized.save(f, "JPEG", quality=85)
+
+    return True
 
 
 if __name__ == '__main__':
@@ -67,19 +97,17 @@ if __name__ == '__main__':
 
     ensure_directory('./images/')
 
-    query_directory = './images/' + args.query + "/"
+    query_directory = './images/' + args.label + "/"
     ensure_directory(query_directory)
 
     image_urls = fetch_image_urls(args.query, args.count)
 
-    image_urls = [url for url in image_urls]
+    values = [item for item in zip(itertools.cycle([query_directory]), image_urls)]
 
-    print(image_urls)
     print("image count", len(image_urls))
 
     pool = Pool(12)
-    pool.map(persist_image, image_urls)
-    persist_image(image_urls[0])
-
+    results = pool.map(persist_image, values)
+    print("Images downloaded: ", len([r for r in results if r]))
 
 
