492713cb0050c018e8d98dc5e8106e5b6b592ae4
mensahdann <mensahd1@niksula.hut.fi>
2016-11-26 16:15:46 +0200

    Image sent to /detect gets recognized and added to inventory if in food list

--- src/server.py
+++ src/server.py
@@ -3,7 +3,9 @@ from flask import Flask, Response
 from flask import request
 from flask import jsonify
 import json
-from src.tagger.photoTagger import jsonParser
+from src.tagger.photo_tagger import recognize
+import time
+
 
 application = Flask(__name__)
 
@@ -14,9 +16,11 @@ def hello_world():
 
 @application.route('/detect', methods=['POST'])
 def parse_image():
-    f = request.files['image']
-    f.save('tagger/uploaded_file.jpeg')
-    return Response(status=200, response='Detected')
+    f = request.files['file']
+    filename = 'tagger/images/uploaded_file' + str(time.time()) + '.jpeg'
+    f.save(filename)
+    recognize(filename)
+    return Response(status=200, response='Recognized')
 
 @application.route('/inventory', methods=['POST'])
 def add_to_inventory():
