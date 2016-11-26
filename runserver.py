ddfd4d82e99ccde706f47bd124f3ebd96b3fc97b
Kenneth Blomqvist <kekeblom@gmail.com>
2016-11-26 11:15:10 +0200

    fix python version, web.config

--- runserver.py
+++ runserver.py
@@ -6,9 +6,9 @@ from os import environ
 from src.server import app
 
 if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+
+    httpd = make_server('localhost', 5555, app)
+    httpd.serve_forever()
     HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+
