dca470f1277284be5c1741c707d4b0ecffb9ac78
Kenneth Blomqvist <kekeblom@gmail.com>
2016-11-26 15:55:16 +0200

    add mock endpoint for getting inventory

--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ from flask import Flask, Response
 from flask import request
 from flask import jsonify
 import json
-from src.tagger.photoTagger import jsonParser
+from tagger.photoTagger import jsonParser
 
 application = Flask(__name__)
 
@@ -18,18 +18,25 @@ def parse_image():
     f.save('tagger/uploaded_file.jpeg')
     return Response(status=200, response='Detected')
 
+@application.route('/inventory', methods=['GET'])
+def get_inventory():
+    with open('src/tagger/inventory.txt', 'r') as f:
+        inventory = f.read()
+        items = [item.strip() for item in inventory.split("\n") if item]
+        return Response(status=200, response=json.dumps(items), content_type='application/javascript')
+
 @application.route('/inventory', methods=['POST'])
 def add_to_inventory():
     # f = request.files['image']
     # f.save('tagger/uploaded_file.jpeg')
     # Mock json files containing responses from Vision API
-    with open('tagger/mock_jsons/tomato.json') as json_data:
+    with open('src/tagger/mock_jsons/tomato.json') as json_data:
         data = json.load(json_data)
         jsonParser(data)
-    with open('tagger/mock_jsons/strawberry.json') as json_data:
+    with open('src/tagger/mock_jsons/strawberry.json') as json_data:
         data = json.load(json_data)
         jsonParser(data)
-    with open('tagger/mock_jsons/cucumber.json') as json_data:
+    with open('src/tagger/mock_jsons/cucumber.json') as json_data:
         data = json.load(json_data)
         jsonParser(data)
     return Response(status=200, response='Added')
--- src/test.py
+++ src/test.py
@@ -1,18 +1,25 @@
 import unittest
-from server import app
+from server import application
+import json
 
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
+        application.config['TESTING'] = True
+        self.app = application.test_client()
 
-    def test_image_post(self):
-        with open('./src/fixtures/dollar.jpg', 'rb') as f:
-            image = f.read()
-            response = self.app.post('/detect', data=image)
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data, b'tomato')
+    # def test_image_post(self):
+    #     with open('./src/fixtures/dollar.jpg', 'rb') as f:
+    #         image = f.read()
+    #         response = self.app.post('/detect', data=image)
+    #         self.assertEqual(response.status_code, 200)
+    #         self.assertEqual(response.data, b'tomato')
+
+    def test_inventory_get(self):
+        response = self.app.get('/inventory')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode("utf-8"))
+        self.assertEqual(len(data), 2)
 
 
 if __name__ == '__main__':
