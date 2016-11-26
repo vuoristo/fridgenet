596e2b8b74474dc4f6c107e0a217061b1cf8f9c0
Kenneth Blomqvist <kekeblom@gmail.com>
2016-11-26 13:58:51 +0200

    gunicorn setup

--- runserver.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-from src.server import app
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
--- src/server.py
+++ src/server.py
@@ -3,14 +3,14 @@ from flask import Flask, Response
 from flask import request
 from flask import jsonify
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/test', methods=['GET'])
+@application.route('/test', methods=['GET'])
 def hello_world():
     return Response(status=200, response='Hello, world!')
 
 
-@app.route('/detect', methods=['POST'])
+@application.route('/detect', methods=['POST'])
 def parse_image():
     list = [
         {'param': 'foo', 'val': 2},
